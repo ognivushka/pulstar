77902c34b85b5603b9cf9177ace28b7194532256
Valeria Markevich <v.markevichhh@gmail.com>
2023-01-10 15:49:03 +0100

    feat: start to emplement add to shoping cart

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
-const btnNavEl = document.querySelector(".button-mobile");
-const headerEl = document.querySelector("header");
+//MOBILE MENU
 
+               //variables
+const btnNavEl = document.querySelector(".button-mobile"),
+ headerEl = document.querySelector("header");
+
+              //event listner
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+////////////////////////////////////////////////////////////////////////////////////
+//SHOPING-LIST
+
+              //variables
+const products = document.querySelector('.shop-products'), //add products to DOM
+shopingCartContent = document.querySelector('.shop-content tbody'),
+clearCartBtn = document.querySelector('.clear-cart')
+
+
+
+               //event listners
+loadEventListeners()
+function loadEventListeners(){
+  products.addEventListener('click', buyProduct)
+}
+
+
+
+
+
+                //functions
+  function buyProduct(e){
+    e.preventDefault();
+    if(e.target.classList.contains('add-to-cart')){
+      const product = e.target
+    }
+    getCourseInfo()
+  }
+
+  function getCourseInfo(product){
+    const productInfo ={
+      image: product.querySelector('img').src,
+      title: product.querySelector('.product-name').textContent,
+      price: product.querySelector('.product-price').textContent,
+      id: product.querySelector('a').getAttribute('data-id'),
+    }
+    console.log(productInfo);
+  }
