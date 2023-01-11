69965a8926fa1c5941991e6e218a87f51d6f7743
Valeria Markevich <v.markevichhh@gmail.com>
2023-01-11 13:41:43 +0100

    fix: add to shoping card

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ btnNavEl.addEventListener("click", function () {
               //variables
 const products = document.querySelector('.shop-products'), //add products to DOM
 shopingCartContent = document.querySelector('.shop-content tbody'),
-clearCartBtn = document.querySelector('.clear-cart')
+clearCartBtn = document.querySelector('.clear-cart'),
+productName = document.querySelector('.product-name')
 
 
 
@@ -33,16 +34,31 @@ function loadEventListeners(){
     e.preventDefault();
     if(e.target.classList.contains('add-to-cart')){
       const product = e.target
+    // console.log(product);
+    getProductInfo(product)
     }
-    getCourseInfo()
+   
   }
 
-  function getCourseInfo(product){
+  function getProductInfo(product){
     const productInfo ={
-      image: product.querySelector('img').src,
+      image: product.querySelector('img'),
       title: product.querySelector('.product-name').textContent,
       price: product.querySelector('.product-price').textContent,
-      id: product.querySelector('a').getAttribute('data-id'),
+      id: product.querySelector('div').getAttribute('data-id'),
     }
-    console.log(productInfo);
+    addIntoCart(productInfo)
+  }
+
+  function addIntoCart(product){
+    const row = document.createElement('tr');
+    row.innerHTML =`
+      <tr>
+        <td>${product.title}</td>
+        <td>${product.price}</td>
+        <td>
+            <a href='#' class="remove" data-id="${product.id}">X</a>
+        </td>
+    `;
+    shopingCartContent.appendChild(row)
   }
